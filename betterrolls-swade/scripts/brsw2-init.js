59495493c7b5c384d8f6673b1853142dae77d6d3
javier <javier@castroparga.com>
2020-11-12 16:25:56 +0100

    Click settings

--- betterrolls-swade/scripts/brsw2-init.js
+++ betterrolls-swade/scripts/brsw2-init.js
@@ -11,7 +11,8 @@ Hooks.on(`ready`, () => {
 	// Create a base object to hook functions
     game.brsw = {};
     attribute_card_hooks();
-	register_settings();
+	register_settings_version2();
+    register_settings();
 })
 
 Hooks.on('renderChatMessage', (message, html) => {
@@ -21,3 +22,47 @@ Hooks.on('renderChatMessage', (message, html) => {
         activate_common_listeners(message, html);
     }
 });
+
+
+// Settings
+
+function register_settings_version2() {
+    const br_choices = {
+        system: 'Default system roll', card: 'Show Betterrolls card'};
+    game.settings.register('betterrolls-swade', 'click', {
+        name: 'Single click action',
+        hint: "Select what happens when you single click a name",
+        default: "system",
+        scope: "world",
+        type: String,
+        choices: br_choices,
+        config: true
+    });
+    game.settings.register('betterrolls-swade', 'shift_click', {
+        name: 'Shift click action',
+        hint: "Select what happens when you click a name while pressing SHIFT",
+        default: "card",
+        scope: "world",
+        type: String,
+        choices: br_choices,
+        config: true
+    });
+    game.settings.register('betterrolls-swade', 'ctrl_click', {
+        name: 'Control click action',
+        hint: "Select what happens when you click a name while pressing Control",
+        default: "card",
+        scope: "world",
+        type: String,
+        choices: br_choices,
+        config: true
+    });
+    game.settings.register('betterrolls-swade', 'alt_click', {
+        name: 'Alt click action',
+        hint: "Select what happens when you click a name while pressing Alt",
+        default: "card",
+        scope: "world",
+        type: String,
+        choices: br_choices,
+        config: true
+    });
+}
