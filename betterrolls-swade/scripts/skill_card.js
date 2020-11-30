7d4a5bde28ad52833319e3d32136bb63ae9e63a6
javier <javier@castroparga.com>
2020-11-30 22:54:03 +0100

    Description in skill card.

--- betterrolls-swade/scripts/skill_card.js
+++ betterrolls-swade/scripts/skill_card.js
@@ -19,7 +19,8 @@ async function create_skill_card(origin, skill_id) {
     let chatData = create_basic_chat_data(actor, CONST.CHAT_MESSAGE_TYPES.IC);
     let render_object = create_render_options(
         actor, {actor: actor, header: {type: 'Skill', title: skill.name,
-            notes: notes, img: skill.img}, footer: footer})
+            notes: notes, img: skill.img}, footer: footer,
+            description: skill.data.data.description})
     chatData.content = await renderTemplate(
         "modules/betterrolls-swade/templates/skill_card.html", render_object);
     let message = await ChatMessage.create(chatData);
