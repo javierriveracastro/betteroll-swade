43f6ea0a72d34ecea3bd02e4fae7819d73320ec2
javier <javier@castroparga.com>
2020-12-02 15:59:31 +0100

    More options with rof in skill card.

--- betterrolls-swade/scripts/brsw2-init.js
+++ betterrolls-swade/scripts/brsw2-init.js
@@ -20,7 +20,8 @@ Hooks.on(`ready`, () => {
     // Load partials.
     const templatePaths = ['modules/betterrolls-swade/templates/common_card_header.html',
         'modules/betterrolls-swade/templates/common_card_footer.html',
-        'modules/betterrolls-swade/templates/common_options.html'];
+        'modules/betterrolls-swade/templates/common_options.html',
+        'modules/betterrolls-swade/templates/common_more_options.html'];
     loadTemplates(templatePaths).then(console.log(
         "Better Rolls templates preloaded"));
     // Add some jquery magic to allow binding our functions prior to system
