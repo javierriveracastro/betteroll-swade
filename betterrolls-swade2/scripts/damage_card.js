e81e15551ad7de8cd674eab0f8e4ab306f4eac11
javier <javier@castroparga.com>
2021-01-28 17:51:21 +0100

    Wait for actor update before processing footer in damage card.

--- betterrolls-swade2/scripts/damage_card.js
+++ betterrolls-swade2/scripts/damage_card.js
@@ -13,23 +13,27 @@ export async function create_damage_card(token_id, damage, damage_text) {
     let token = canvas.tokens.get(token_id);
     let actor = token.actor;
     let user = get_owner(actor);
-    let text = apply_damage(token, damage);
+    let text = await apply_damage(token, damage);
     let footer = [`${game.i18n.localize("SWADE.Wounds")}: ${actor.data.data.wounds.value}/${actor.data.data.wounds.max}`]
     for (let status in actor.data.data.status) {
+        // noinspection JSUnfilteredForInLoop
         if (actor.data.data.status[status]) {
             footer.push(status.slice(2));
         }
     }
     let trait_roll = new BRWSRoll();
-        let message = await create_common_card(actor,
-        {header: {type: game.i18n.localize("SWADE.Dmg"),
-                title: game.i18n.localize("SWADE.Dmg"),
-                notes: damage_text}, text: text, footer: footer,
+    let message = await create_common_card(actor,
+    {header: {type: game.i18n.localize("SWADE.Dmg"),
+            title: game.i18n.localize("SWADE.Dmg"),
+            notes: damage_text}, text: text, footer: footer,
             trait_roll: trait_roll}, CONST.CHAT_MESSAGE_TYPES.IC,
-        "modules/betterrolls-swade2/templates/damage_card.html")
+    "modules/betterrolls-swade2/templates/damage_card.html")
     await message.setFlag('betterrolls-swade2', 'card_type',
         BRSW_CONST.TYPE_DMG_CARD)
     return message
+    // TODO: Undo damage
+    // TODO: Soak rolls
+    // TODO: Remove conditions from footer???
 }
 
 
@@ -53,7 +57,7 @@ function get_owner(actor) {
  * @param token
  * @param damage
  */
-function apply_damage(token, damage) {
+async function apply_damage(token, damage) {
     if (damage < 0) return;
     if (!token.hasOwnProperty('actor')) {
         // If this is not a token then it is a token id
@@ -67,11 +71,11 @@ function apply_damage(token, damage) {
     }
     const final_wounds = token.actor.data.data.wounds.value + wounds;
     if (final_wounds > token.actor.data.data.wounds.max) {
-        token.actor.update({'data.wounds.value': token.actor.data.data.wounds.max});
+        await token.actor.update({'data.wounds.value': token.actor.data.data.wounds.max});
     } else {
-        token.actor.update({'data.wounds.value': final_wounds});
+        await token.actor.update({'data.wounds.value': final_wounds});
     }
-    token.actor.update({'data.status.isShaken': true});
+    await token.actor.update({'data.status.isShaken': true});
     // noinspection JSIgnoredPromiseFromCall
     return  wounds ? `${wounds} wound(s) has been added to ${token.name}` :
             `${token.name} is now shaken`;
