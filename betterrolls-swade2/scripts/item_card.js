3b192fc070016da00eea0011e409a7747f34ab84
javier <javier@castroparga.com>
2021-03-20 13:05:49 +0100

    Show better rolls card on clicks on the item name on quick access.

--- betterrolls-swade2/scripts/item_card.js
+++ betterrolls-swade2/scripts/item_card.js
@@ -173,7 +173,7 @@ async function item_click_listener(ev, target) {
  */
 export function activate_item_listeners(app, html) {
     let target = app.token?app.token:app.object;
-    const item_images = html.find('.item-image, .item-img, .item.flexrow > img, .item-show, span.item>.item-control.item-edit');
+    const item_images = html.find('.item-image, .item-img, .item.flexrow > img, .item-show, span.item>.item-control.item-edit, .item-name');
     item_images.bindFirst('click', async ev => {
         await item_click_listener(ev, target);
     });
