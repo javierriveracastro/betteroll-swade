a8f72c71c3f2866c7cbf020d5d7e8f7c7859817e
javier <javier@castroparga.com>
2021-04-23 16:55:07 +0200

    Community sheet fixes.

--- betterrolls-swade2/scripts/item_card.js
+++ betterrolls-swade2/scripts/item_card.js
@@ -188,7 +188,7 @@ async function item_click_listener(ev, target) {
  */
 export function activate_item_listeners(app, html) {
     let target = app.token?app.token:app.object;
-    const item_images = html.find('.item-image, .item-img, .item.flexrow > img, .item-show, span.item>.item-control.item-edit, .gear-card>.card-header>.item-name, .damage-roll, .item>.item-name, .power-header>.item-name');
+    const item_images = html.find('.item-image, .item-img, .item-show, span.item>.item-control.item-edit, .gear-card>.card-header>.item-name, .damage-roll, .item-name>h4, .power-header>.item-name');
     item_images.bindFirst('click', async ev => {
         await item_click_listener(ev, target);
     });
