1b2df10a82b044c9236f05fdf4b91487b28f7557
javier <javier@castroparga.com>
2021-08-13 18:00:23 +0200

    Hopefully more accesible health bars.

--- betterrolls-swade2/scripts/tokenbars.js
+++ betterrolls-swade2/scripts/tokenbars.js
@@ -5,14 +5,15 @@ export function modifyTokenBars() {
 
     Token.prototype._drawBar = function (number, bar, data) {
         const val = Number(data.value);
+        // noinspection JSUnresolvedFunction
         const pct = Math.clamped(val, 0, data.max) / data.max;
         let h = Math.max((canvas.dimensions.size / 12), 8);
         if (this.data.height >= 2) h *= 1.6;  // Enlarge the bar for large tokens
         // Draw the bar
-        let color = [0.8, (1 - pct), (0)];
+        let color = [pct, 0.8 * (1 - pct), 0]
         bar.clear()
-            .beginFill(0x005500, 1)
-            .lineStyle(1, 0x000000, 1);
+            .beginFill(0x005500, 0)
+            .lineStyle(3, 0x000000, 1);
         // each max draw a green rectangle in background
         for (let index = 0; index < data.max; index++) {
             bar.drawRect(index * (this.w / data.max), 0, (this.w / data.max), h);
