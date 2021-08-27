a509c5d4b3bfc0147db2d95748c2d5f15d972c30
javier <javier@castroparga.com>
2021-08-27 16:22:34 +0200

    Accept self_add_status to allowed keywords for custom actions.

--- betterrolls-swade2/scripts/global_actions.js
+++ betterrolls-swade2/scripts/global_actions.js
@@ -306,7 +306,7 @@ export class WorldGlobalActions extends FormApplication {
                 'dmgOverride', 'defaultChecked', 'runSkillMacro', 'runDamageMacro',
                 'raiseDamageFormula', 'wildDieFormula', 'rerollSkillMod', 'rerollDamageMod',
                 'selector_type', 'selector_value', 'and_selector', 'group', 'shotsUsed',
-                'or_selector', 'rof']
+                'or_selector', 'rof', 'self_add_status']
             for (let key in action) {
                 if (SUPPORTED_KEYS.indexOf(key) < 0) {
                     error = game.i18n.localize("BRSW.UnknownActionKey") + key
