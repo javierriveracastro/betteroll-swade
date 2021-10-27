51628a67aae23a0cc5d42d548c853bbf9a8507f0
javier <javier@castroparga.com>
2021-10-27 17:14:10 +0200

    Make items use the new drag system (unusable from quickstart)

--- betterrolls-swade2/scripts/item_card.js
+++ betterrolls-swade2/scripts/item_card.js
@@ -201,8 +201,9 @@ export function activate_item_listeners(app, html) {
     item_images.bindFirst('click', async ev => {
         await item_click_listener(ev, target);
     });
-    let item_li = html.find('.gear-card.item, .item.flexrow, .power.item, .weapon.item')
-    item_li.attr('draggable', 'true');
+    // TODO: Make the icons in the quick-access dragable
+    // let item_li = html.find('.gear-card.item, .item.flexrow, .power.item, .weapon.item')
+    // item_li.attr('draggable', 'true');
 }
 
 
