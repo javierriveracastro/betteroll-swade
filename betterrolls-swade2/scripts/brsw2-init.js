4a7aaececbbcbe0b3f02aab9724a7bc55353bec1
javier <javier@castroparga.com>
2022-02-21 23:24:23 +0100

    Use foundry.utils.getRoute to import dsn avoiding problems with The Forge.

--- betterrolls-swade2/scripts/brsw2-init.js
+++ betterrolls-swade2/scripts/brsw2-init.js
@@ -1,5 +1,5 @@
 // Init scripts for version 2
-/* globals Hooks, console, game, loadTemplates, Token, renderTemplate, Macro, CONFIG */
+/* globals Hooks, console, game, loadTemplates, Token, renderTemplate, Macro, CONFIG, foundry */
 import {activate_common_listeners, manage_selectable_click, manage_collapsables,
     BRSW_CONST, get_action_from_click} from './cards_common.js';
 import {attribute_card_hooks, activate_attribute_listeners,
@@ -404,7 +404,7 @@ function register_settings_version2() {
 
 function register_dsn_settings(){
     // noinspection JSFileReferences
-    import('../../dice-so-nice/DiceColors.js').then(dsn => {
+    import(foundry.utils.getRoute('/modules/dice-so-nice/DiceColors.js')).then(dsn => {
         let theme_choice = {};
         // noinspection JSUnresolvedVariable
         for (let theme in dsn.COLORSETS) {
