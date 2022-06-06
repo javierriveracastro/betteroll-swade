d4197b05e9edf8dc7205b2fd568d186d20a90ac6
Mike Bloy <mike@bloy.org>
2022-06-05 20:10:57 -0500

    only owner creates unshake chat card
    Add a check to prevent n unshake chat cards from being created where n
    is the number of users logged in.

--- betterrolls-swade2/scripts/combat.js
+++ betterrolls-swade2/scripts/combat.js
@@ -2,11 +2,17 @@
 /* globals canvas */
 
 import {create_unshaken_card} from "./unshake_card.js";
+import {get_owner} from "./damage_card.js";
+
 
 export async function round_start(combat){
     const token_id = combat.current.tokenId
     const actor = canvas.tokens.get(token_id).actor
     if (actor.data.data.status.isShaken) {
-        await create_unshaken_card(undefined, token_id)
+        let owner = get_owner(actor);
+        console.log("userid", owner.id, "game user", game.user.id);
+        if (owner.id === game.user.id) {
+            await create_unshaken_card(undefined, token_id);
+        }
     }
 }
