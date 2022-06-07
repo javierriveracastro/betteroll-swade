4cad05034d7d9acca73512d41aec5ac55f4522c6
javier <javier@castroparga.com>
2022-06-07 16:24:36 +0200

    Assure there is an actor before checking if it is shaken at the start of a combat round.

--- betterrolls-swade2/scripts/combat.js
+++ betterrolls-swade2/scripts/combat.js
@@ -6,7 +6,7 @@ import {create_unshaken_card} from "./unshake_card.js";
 export async function round_start(combat){
     const token_id = combat.current.tokenId
     const actor = canvas.tokens.get(token_id).actor
-    if (actor.data.data.status.isShaken) {
+    if (actor && actor.data.data.status.isShaken) {
         const first_gm = game.users.filter(u => u.isGM)[0]
         if (game.user.id === first_gm.id) {
             await create_unshaken_card(undefined, token_id);
