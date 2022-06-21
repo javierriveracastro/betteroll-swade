698112e941f12b31d074adec04cf2a988a5d5a19
javier <javier@castroparga.com>
2022-06-21 17:41:41 +0200

    Make reloading break less with space problems.

--- betterrolls-swade2/scripts/item_card.js
+++ betterrolls-swade2/scripts/item_card.js
@@ -796,7 +796,7 @@ export async function roll_item(message, html, expend_bennie,
 function reload_weapon(actor, weapon, number) {
     // If the quantity of ammo is less than the amount required, use whatever is left.
     let item = actor.items.get(weapon.id);
-    let ammo = actor.items.getName(item.data.data.ammo)
+    let ammo = actor.items.getName(item.data.data.ammo.trim())
     let ammo_quantity = 999999999;
     if (ammo) {
         if (ammo.data.data.quantity <= 0) {
