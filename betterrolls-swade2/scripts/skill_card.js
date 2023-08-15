aeae0b08e1441020982eb26251b667052c3dc733
javier Rivera <javier@planetakromo.com>
2023-08-15 17:59:16 +0200

    Check that extrema range is truth.

--- betterrolls-swade2/scripts/skill_card.js
+++ betterrolls-swade2/scripts/skill_card.js
@@ -252,20 +252,19 @@ function calculate_distance(origin_token, target_token, item, tn, skill) {
                 }
             }
         }
-        let extreme_ranger = 0
+        let extreme_range = 0
         for (let i = 0; i < 3 && i < range.length; i++) {
             let range_int = parseInt(range[i])
             if (rangeEffects) {
                 range_int = range_int + rangeEffects * (i + 1)
             }
-            if (range_int > extreme_ranger) {
-                extreme_ranger = range_int
+            if (range_int > extreme_range) {
             }
             if (range_int && range_int < distance) {
                 distance_penalty = i < 2 ? (i + 1) * 2 : 8;
             }
         }
-        if (distance > extreme_ranger * 4) {
+        if (extreme_range && distance > extreme_range * 4) {
             tn.modifiers.push(
                 create_modifier(game.i18n.localize("BRSW.OverExtremeRange"), -999))
         }
