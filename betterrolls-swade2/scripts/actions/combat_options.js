4c4441f97530b69824daa13e957d3cd352c8b461
javier Rivera <javier@planetakromo.com>
2023-09-11 15:33:05 +0200

    Misserably copy Bruno's implementation of Desesperate Attack.

--- betterrolls-swade2/scripts/actions/combat_options.js
+++ betterrolls-swade2/scripts/actions/combat_options.js
@@ -300,4 +300,42 @@ export const COMBAT_OPTIONS = [
     ],
     group: "SWADE.APLong",
   },
+  {
+    id: "DESPERATE_ATTACK-2",
+    name: "Desperate Attack +2",
+    button_name: "Desperate Attack +2",
+    skillMod: 2,
+    dmgMod: -2,
+    dmgOverride: "",
+    and_selector: [
+      {
+        selector_type: "skill",
+        selector_value: "Fighting",
+      },
+      {
+        selector_type: "item_type",
+        selector_value: "weapon",
+      },
+    ],
+    group: "BRSW.AttackOption",
+  },
+  {
+    id: "DESPERATE_ATTACK-4",
+    name: "Desperate Attack +4",
+    button_name: "Desperate Attack +4",
+    skillMod: 4,
+    dmgMod: -4,
+    dmgOverride: "",
+    and_selector: [
+      {
+        selector_type: "skill",
+        selector_value: "Fighting",
+      },
+      {
+        selector_type: "item_type",
+        selector_value: "weapon",
+      },
+    ],
+    group: "BRSW.AttackOption",
+  },
 ];
