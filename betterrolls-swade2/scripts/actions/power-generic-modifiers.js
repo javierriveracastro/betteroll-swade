47804845b4bb0dbe0974307935237ec4b0311782
javier Rivera <javier@planetakromo.com>
2023-09-25 15:44:27 +0200

    Add translations to generic power modifiers.

--- betterrolls-swade2/scripts/actions/power-generic-modifiers.js
+++ betterrolls-swade2/scripts/actions/power-generic-modifiers.js
@@ -1,117 +1,117 @@
 /// generic power modifiers for any power ...
 
-const groupNameGenericPModifiers = "Power Modifiers - Generic";
+const groupNameGenericPModifiers = "BRSW.PowerModifiersGeneric";
 
 export const GENERIC_POWER_MODIFIERS = [
   {
     id: "ARMORPIERCING1",
-    name: "Armor Piercing 2 (+1)",
-    button_name: "Armor Piercing 2 (+1)",
+    name: "BRSW.ArmorPiercing1",
+    button_name: "BRSW.ArmorPiercing1",
     overrideAp: "2",
     shotsUsed: "+1",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "ARMORPIERCING2",
-    name: "Armor Piercing 4 (+2)",
-    button_name: "Armor Piercing 4 (+2)",
+    name: "BRSW.ArmorPiercing2",
+    button_name: "BRSW.ArmorPiercing2",
     overrideAp: "4",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "ARMORPIERCING3",
-    name: "Armor Piercing 6 (+3)",
-    button_name: "Armor Piercing 6 (+3)",
+    name: "BRSW.ArmorPiercing3",
+    button_name: "BRSW.ArmorPiercing3",
     overrideAp: "6",
     shotsUsed: "+3",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "FATIGUE",
-    name: "Fatigue (+2)",
-    button_name: "Fatigue (+2)",
+    name: "BRSW.FatigueModifier",
+    button_name: "BRSW.FatigueModifier",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "GLOWSHROUD1",
-    name: "Glow/Shroud (+1)",
-    button_name: "Glow/Shroud (+1)",
+    name: "BRSW.GlowShroud",
+    button_name: "BRSW.GlowShroud",
     shotsUsed: "+1",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "HEAVYWEAPON2",
-    name: "Heavy Weapon (+2)",
-    button_name: "Heavy Weapon (+2)",
+    name: "BRSW.HeavyWeaponModifier",
+    button_name: "BRSW.HeavyWeaponModifier",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "HINDERHURRY1",
-    name: "Hinder/Hurry (+1)",
-    button_name: "Hinder/Hurry (+1)",
+    name: "BRSW.HinderHurryModifier",
+    button_name: "BRSW.HinderHurryModifier",
     shotsUsed: "+1",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "LINGERINGDAMAGE2",
-    name: "Lingering Damage (+2)",
-    button_name: "Lingering Damage (+2)",
+    name: "BRSW.LingeringDamageModifier",
+    button_name: "BRSW.LingeringDamageModifier",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "RANGE1",
-    name: "Range 2x (+1)",
-    button_name: "Range 2x (+1)",
+    name: "BRSW.RangeModifier1",
+    button_name: "BRSW.RangeModifier1",
     shotsUsed: "+1",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "RANGE2",
-    name: "Range 3x (+2)",
-    button_name: "Range 3x (+2)",
+    name: "BRSW.RangeModifier2",
+    button_name: "BRSW.RangeModifier2",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "SELECTIVE1",
-    name: "Selective (+1)",
-    button_name: "Selective (+1)",
+    name: "BRSW.SelectiveModifier",
+    button_name: "BRSW.SelectiveModifier",
     shotsUsed: "+1",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
+    group: groupNameGenericPModifiers,
   },
   {
     id: "HASTY2",
-    name: "Hasty (+2)",
-    button_name: "Hasty (+2)",
+    name: "BRSW.HastyModifier",
+    button_name: "BRSW.HastyModifier",
     shotsUsed: "+2",
     selector_type: "item_type",
     selector_value: "power",
-    group: groupNameGenericPModifiers
-  }
+    group: groupNameGenericPModifiers,
+  },
 ];
