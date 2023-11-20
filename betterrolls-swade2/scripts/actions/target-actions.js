659b979d7600bfe59b9a30713a1a23fb35ce5161
Steve Culshaw <steveculshaw@gmail.com>
2023-11-20 17:04:36 +0000

    move target having dodge to target actions

--- betterrolls-swade2/scripts/actions/target-actions.js
+++ betterrolls-swade2/scripts/actions/target-actions.js
@@ -3,4 +3,56 @@
 //
 
 export const TARGET_ACTIONS = [
+  {
+    id: "Target has dodge 1",
+    name: "BRSW.TargetHasDodge",
+    button_name: "BRSW.TargetHasDodge",
+    skillMod: "-2",
+    and_selector: [
+      {
+        selector_type: "item_type",
+        selector_value: "weapon"
+      },
+      {
+        selector_type: "target_has_edge",
+        selector_value: "BRSW.EdgeName-Dodge",
+      },
+      {
+        or_selector: [
+          {
+            selector_type: "skill",
+            selector_value: "BRSW.SkillName-UnskilledAttempt"
+          },
+          {
+            selector_type: "skill",
+            selector_value: "BRSW.SkillName-Shooting"
+          },
+          {
+            selector_type: "skill",
+            selector_value: "BRSW.SkillName-Athletics"
+          },
+        ],
+      },
+    ],
+    defaultChecked: "on",
+    group: "BRSW.Target",
+  },
+  {
+    id: "Target has dodge 2",
+    name: "BRSW.TargetHasDodgePower",
+    button_name: "BRSW.TargetHasDodgePower",
+    skillMod: "-2",
+    and_selector: [
+      {
+        selector_type: "target_has_edge",
+        selector_value: "BRSW.EdgeName-Dodge",
+      },
+      {
+        selector_type: "item_type",
+        selector_value: "power"
+      },
+    ],
+    defaultChecked: "on",
+    group: "BRSW.Target",
+  },
 ];
