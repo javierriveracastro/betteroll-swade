2d870d5735a8d44f7d5a9562cbaad099faaa0956
javier Rivera <javier@planetakromo.com>
2024-05-29 16:01:22 +0200

    Support the new item action to override AP.

--- betterrolls-swade2/scripts/actions.js
+++ betterrolls-swade2/scripts/actions.js
@@ -3,7 +3,7 @@
 import { broofa } from "./utils.js";
 
 export class brAction {
-  constructor(name, code, type='', idOverride=0) {
+  constructor(name, code, type = "", idOverride = 0) {
     this.name = name;
     if (type === "item") {
       this.code = JSON.parse(JSON.stringify(code));
@@ -11,13 +11,17 @@ export class brAction {
     } else {
       this.code = code;
     }
-    if (idOverride != 0) {
+    if (idOverride !== 0) {
       this.code.id = idOverride;
     }
     this.selected = false;
     this.recreate_skill_damage_mods();
     this.has_skill_mod = !!(this.code.skillMod || this.code.skillOverride);
-    this.has_damage_mod = !!(this.code.dmgMod || this.code.dmgOverride);
+    this.has_damage_mod = !!(
+      this.code.dmgMod ||
+      this.code.dmgOverride ||
+      this.code.overrideAp
+    );
   }
 
   recreate_skill_damage_mods() {
@@ -33,6 +37,9 @@ export class brAction {
         this.code.dmgMod = this.code.modifier;
         this.code.dmgOverride = this.code.override;
       }
+    }
+    if (this.code.hasOwnProperty("ap")) {
+      this.code.overrideAp = this.code.ap;
     }
   }
 }
